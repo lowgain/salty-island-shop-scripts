507d5c713d8e923d8dc435b8293d6772c8177001
lowgain <logan.t2020@tutanota.com>
2025-10-07 01:49:04 +0000

    add devenv

--- weekly-sales/weekly-sales.py
+++ weekly-sales/weekly-sales.py
@@ -1,13 +1,28 @@
+import sys
 import csv
+import datetime
+import logging
 
-ItemList = []
-print('Press Enter to move to a newline')
 
-with open('sales-summary-2024-02-01-2024-12-31 3.csv', newline='') as CSVFile:
-  FileReader = csv.DictReader(CSVFile, delimiter=',')
-  for i in FileReader:
-    ItemList.append(i)
+def main():
+    if not sys.argv[1].endswith(".csv"):
+        raise TypeError("File MUST be a CSV")
+    input_file = csv.DictReader(open(sys.argv[1], "r", newline=""), delimiter=",")
+    print("Press Enter to move to a newline")
+    print(f"| {'Date':<17}|{'Gross sales':>12} |")
+    for item in input_file:
+        input(f"| {item['Date']:<17}|{'$' + item['Gross sales']:>12} |")
 
-print(f'| {"Date":<17}|{"Gross sales":>12} |')
-for i in reversed(ItemList):
-  input(f'| {i["Date"]:<17}|{'$' + i["Gross sales"]:>12} |')
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logging.basicConfig(
+            filename=f"{datetime.datetime.now()}.log",
+            encoding="utf-8",
+            level=logging.DEBUG,
+        )
+        print(e)
+        logger.exception(e)
